0939fbf1ad94b6f711b590e21acdad1ce15cf91e
Rania Aqila <rania.aqila51@ui.ac.id>
2026-09-14 20:57:04 +0700

    feat(works): tambah view dan routing URL untuk halaman works

--- main/views.py
+++ main/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Work
 
 
 def show_main(request):
     context = {
-        "name": "nia",
+        "name": "Rania Aqila",
+        "nickname": "nia",
         "npm": "2506623282",
         "study_program": "S1 Ilmu Komputer",
         "bio": (
@@ -24,3 +25,13 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_works(request):
+    context = {
+        "name": "Rania Aqila",
+        "event_management": Work.objects.filter(category="event_management").order_by("-year"),
+        "writing": Work.objects.filter(category="writing").order_by("-year"),
+        "business_case": Work.objects.filter(category="business_case").order_by("-year"),
+        "product_management": Work.objects.filter(category="product_management").order_by("-year"),
+    }
+    return render(request, "works.html", context)
