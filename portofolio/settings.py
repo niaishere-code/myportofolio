4ec9821df5755f0488b23d32dbd05dd919876cfd
Rania Aqila <rania.aqila51@ui.ac.id>
2026-09-16 23:12:12 +0700

    chore: perbarui konfigurasi project dan dependensi

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -147,3 +147,5 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["https://rania-aqila51-myportofolio.pws.cs.ui.ac.id"]
