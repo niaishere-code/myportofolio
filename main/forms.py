f65c1f6c22019674150f5adce486ac670132f48a
Rania Aqila <rania.aqila51@ui.ac.id>
2026-09-21 20:13:47 +0700

    feat(forms): menambahkan kelas form untuk pembuatan model experience

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm, TextInput, NumberInput, URLInput, Select
+from django.forms.widgets import Textarea
 
-from main.models import Work
+from main.models import Work, Experience
 
 
 class WorkForm(ModelForm):
@@ -34,3 +35,40 @@ class WorkForm(ModelForm):
                 "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
             }),
         }
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = ["title", "organization", "period", "description", "category"]
+
+        labels = {
+            "title": "Judul",
+            "organization": "Organisasi",
+            "period": "Periode",
+            "description": "Deskripsi",
+            "category": "Kategori",
+        }
+
+        widgets = {
+            "title": TextInput(attrs={
+                "placeholder": "Asisten Dosen Kalkulus 1",
+                "maxlength": 255,
+            }),
+            "organization": TextInput(attrs={
+                "placeholder": "Fakultas Ilmu Komputer UI",
+                "maxlength": 255,
+            }),
+            "period": TextInput(attrs={
+                "placeholder": "Jan 2025 - Sekarang",
+                "maxlength": 100,
+            }),
+            "description": Textarea(attrs={
+                "placeholder": "Ceritakan pengalamanmu",
+                "rows": 4,
+            }),
+            "category": Select(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["category"].empty_label = None
